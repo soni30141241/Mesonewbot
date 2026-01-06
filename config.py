8222640152ffae03cc9c29d6424e3fd77a0608ba
soni30141241 <soni30141241@gmail.com>
2026-01-06 16:49:11 +0530

    Update config.py
    Signed-off-by: soni30141241 <soni30141241@gmail.com>

--- config.py
+++ config.py
@@ -44,8 +44,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "http://api.nexgenbots.xyz")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/randomlychats")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+hyjl0Mp7DzU4NTA1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+hyjl0Mp7DzU4NTA1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
